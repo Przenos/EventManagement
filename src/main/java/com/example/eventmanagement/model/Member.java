7a3058198bf453d2cd071458c16d6270b114b0e2
Przenos <przemyslaw.noske@gmail.com>
2022-09-25 09:40:05 +0200

    Construktor + getter/setter

--- src/main/java/com/example/eventmanagement/model/Member.java
+++ src/main/java/com/example/eventmanagement/model/Member.java
@@ -1,4 +1,47 @@
 package com.example.eventmanagement.model;
 
 public class Member {
+private String login;
+private String password;
+private String Nick;
+private String mail;
+
+    public Member(String login, String password, String nick, String mail) {
+        this.login = login;
+        this.password = password;
+        Nick = nick;
+        this.mail = mail;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNick() {
+        return Nick;
+    }
+
+    public void setNick(String nick) {
+        Nick = nick;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }
