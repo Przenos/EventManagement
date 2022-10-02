c168ac59e1ab469c3fe97468eb51edff256bdb83
Przenos <przemyslaw.noske@gmail.com>
2022-10-02 09:12:37 +0200

    Would like to lunch it

--- src/main/java/com/example/eventmanagement/controler/IndexControler.java
+++ src/main/java/com/example/eventmanagement/controler/IndexControler.java
@@ -1,13 +1,15 @@
 package com.example.eventmanagement.controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/index")
 public class IndexControler {
-
 
 
 
